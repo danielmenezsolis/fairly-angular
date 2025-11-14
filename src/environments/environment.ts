55453a6572ec2470b34d9e5fc1979077fc15d5d0
danielmenezsolis <35786816+danielmenezsolis@users.noreply.github.com>
2025-11-13 18:19:35 -0600

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://fairlyapi-480808131829.us-central1.run.app/api' 
-};
--- src/environments/environments.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://fairlyapi-480808131829.us-central1.run.app/api'  // Tu URL de Cloud Run
-};
